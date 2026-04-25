5847841664ee175ce973ad02a56954d92d6d957b
biologist642 <zhyaw.511123022@uor.edu.krd>
2026-04-25 21:40:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 window.addEventListener('load', function() {
-    // دوای ٣ چرکە لاپەڕەی بەخێرهاتنەکە ون دەبێت
     setTimeout(function() {
         const loader = document.getElementById('loader');
         if (loader) {
             loader.classList.add('fade-out');
         }
-    }, 3000);
+    }, 3000); // ٣ چرکە بەخێرهاتن
 });
